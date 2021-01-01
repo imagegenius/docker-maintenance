e15bc74db920a2f8f2e0120d5b33e62229a717c9
hydazz <alexanderhyde@icloud.com>
2021-01-01 20:34:33 +1100

    adjustments for new s6

--- update_version_info.sh
+++ update_version_info.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-OVERLAY_VERSION=$(curl -sX GET "https://raw.githubusercontent.com/hydazz/docker-baseimage-alpine/main/version_info.json" | jq -r .overlay_version)
+OVERLAY_VERSION=$(cat package_versions.txt | grep -E "s6-overlay.*?-" | sed -n 1p | cut -c 12- | sed -E 's/-r.*//g')
 NGINX_VERSION=$(cat package_versions.txt | grep -E "nginx.*?-" | sed -n 1p | cut -c 7- | sed -E 's/-r.*//g')
+
 OLD_OVERLAY_VERSION=$(cat version_info.json | jq -r .overlay_version)
 OLD_NGINX_VERSION=$(cat version_info.json | jq -r .nginx_version)
 
